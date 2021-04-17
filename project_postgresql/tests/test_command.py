b5e9f9808f784e0dca4b7091d8791db35333bb32
righ <django.hamster@gmail.com>
2021-04-17 17:41:55 +0900

    b10

--- project_postgresql/tests/test_command.py
+++ project_postgresql/tests/test_command.py
@@ -6,3 +6,9 @@ def test_sqla_codegen():
     call_command("sqla_codegen")
     import models_sqla
     importlib.reload(models_sqla)
+
+
+def test_save_original_template():
+    from d2a.template import TEMPLATE
+    with open("./original_template.tmpl", "w") as f:
+        f.write(TEMPLATE)
